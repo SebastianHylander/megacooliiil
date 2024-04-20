9f6ec20b3143ff43e49abb8d5ec1f8d3b84045dd
SebastianHylander <hylandersebastian@gmail.com>
2024-04-20 14:50:24 +0200

    added basic inventory again

---
+++ Megacooliiil/Item.gd
@@ -0,0 +1,20 @@
+extends Panel
+
+var holding : Node
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print(holding)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func add_item(item : Node):
+	holding = item
+	var sprite = item.get_child(0)
+	sprite.scale.x = size.x/sprite.texture.get_width()
+	sprite.scale.y = size.y/sprite.texture.get_height()
+	add_child(item)
