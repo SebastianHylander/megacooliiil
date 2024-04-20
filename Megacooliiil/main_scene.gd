e55782089368d75d09277204d6be4048731d5fe2
CamillaFrank <75301585+CamillaFrank@users.noreply.github.com>
2024-04-20 13:35:48 +0200

    room renaming

--- Megacooliiil/main_scene.gd
+++ Megacooliiil/main_scene.gd
@@ -1,13 +1,23 @@
 extends Node2D
 
-var room1 = preload("res://rooms/first_room.tscn").instantiate()
+var roomStart = preload("res://rooms/start_room.tscn").instantiate()
+var roomJail = preload("res://rooms/jail_room.tscn").instantiate()
+
+var rooms = {
+	"roomStart": roomStart,
+	"roomJail": roomJail
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	add_child(room1)
+	add_child(roomStart)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func change_room(from_room, to_room):
+	remove_child(from_room)
+	add_child(to_room)
