48e1f37649f27026a2138c093f335de618545327
CamillaFrank <75301585+CamillaFrank@users.noreply.github.com>
2024-04-20 20:28:15 +0200

    added falen painting texture

--- Megacooliiil/rooms/break_room.gd
+++ Megacooliiil/rooms/break_room.gd
@@ -7,8 +7,14 @@ var first_enter = true
 func enter(mainnode):
 	if first_enter:
 		first_enter = false
-		var painting = mainnode.get_room("roomPainting").get_node("Room/Painting")
-		painting.locked = false
+		#unlock painting
+		var painting_room = mainnode.get_room("roomPainting")
+		painting_room.get_node("Room/Painting").locked = false
+		
+		#Change painting to fallen
+		var fallen_painting = preload("res://images/painting_room_with_tunnel.png")
+		painting_room.get_node("Room").set_texture(fallen_painting)
+		
 	mainnode.get_node("UILayer/BackArrow").visible = false
 
 func exit(mainnode):
