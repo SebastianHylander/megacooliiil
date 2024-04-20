d7f3cbeacb0956220a37278ea80875d2482a967e
SebastianHylander <hylandersebastian@gmail.com>
2024-04-20 23:57:15 +0200

    SSSSOOOOUUUUNDDSSSS

--- Megacooliiil/rooms/info_object.gd
+++ Megacooliiil/rooms/info_object.gd
@@ -1,9 +1,8 @@
 extends Area2D
 
 @export var message : String
-@export var cell_sound : bool
-
-var uhhSound = preload("res://sounds/uhh.wav")
+@export var has_sound : bool
+@export var sound :  Resource
 var sound_player := AudioStreamPlayer.new()
 
 
@@ -15,6 +14,6 @@ func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		var textfield = get_node('../../../UILayer/MarginContainer/Panel/MarginContainer/Panel/Textfield')
 		textfield.write_text(message)
-		if cell_sound:
-			sound_player.stream = uhhSound
+		if has_sound:
+			sound_player.stream = sound
 			sound_player.play()
--- Megacooliiil/rooms/storage_room.gd
+++ Megacooliiil/rooms/storage_room.gd
@@ -1,12 +1,28 @@
 extends Node2D
 
 @export var prev_room : String
+@export var whisper_streamer : AudioStreamPlayer2D
 var first_enter = true
+var rng = RandomNumberGenerator.new()
+
+var delay : float = 5.0
+var time_spent : float = 0
 
 # Called when the room is entered
 func enter(mainnode):
 	if first_enter:
 		first_enter = false
 
+func _process(delta):
+	time_spent += delta
+	if time_spent > delay:
+		play_sound()
+		delay = rng.randf_range(8.0,18.0)
+		time_spent = 0.0
+	
 func exit(mainnode):
 	pass
+
+func play_sound():
+	whisper_streamer.stream = load("res://sounds/whispers/whisper "+str(rng.randi_range(1,9))+".wav")
+	whisper_streamer.play()
