e0c1531cbefbf27dad080ef8c5e4fe51dcd1091e
SebastianHylander <hylandersebastian@gmail.com>
2024-04-21 02:31:04 +0200

    spade drop sound

--- Megacooliiil/rooms/jail_room.gd
+++ Megacooliiil/rooms/jail_room.gd
@@ -2,11 +2,23 @@ extends Node2D
 
 @export var prev_room : String
 var first_enter = true
+var play_sound = false
+var sound_played = false
 
 # Called when the room is entered
 func enter(mainnode):
+	
 	if first_enter:
 		first_enter = false
-
+	elif not sound_played:
+		play_sound = true
+		sound_played = true
+			
+func _process(delta):
+	if play_sound:
+		get_node('Room/sound_player').play_sound()
+		play_sound = false
+	
 func exit(mainnode):
 	pass
+
---
+++ Megacooliiil/rooms/sound_player.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+
+@export var streamer : AudioStreamPlayer2D
+
+func play_sound():
+	streamer.play()
